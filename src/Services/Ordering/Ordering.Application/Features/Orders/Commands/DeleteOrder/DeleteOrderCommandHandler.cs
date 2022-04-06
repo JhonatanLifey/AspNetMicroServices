f71c89e8259cc0960190f5863d5147d68aded1ea
Jhonatan Lifey <JhonatanLifey@outlook.com>
2022-04-06 12:26:32 +0930

    Order API and Start EventBus

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IMapper mapper, IOrderRepository orderRepository, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+                       
+            if (orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+                //_logger.LogError($"Order {request.Id} not exist on database");
+            }
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully Delete");
+
+            return Unit.Value;
+
+            //throw new System.NotImplementedException();
+        }
+    }
+}
